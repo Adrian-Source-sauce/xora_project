beb6fb4bb506adb93401b34c91ed1bcaf3264097
Adrian <muhadrian2902>
2025-01-05 22:56:38 +0800

    Perubahan di faq

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,9 +1,10 @@
 import clsx from "clsx";
-import React from "react";
 
+import PropTypes from "prop-types"; // Tambahkan import untuk PropTypes
 import { useState } from "react";
 import 'react-slidedown/lib/slidedown.css';
 import SlideDown from "react-slidedown";
+
 const FaqItem = ({ item, index }) => {
   const [activeId, setActiveId] = useState(null);
   const active = activeId === item.id;
@@ -11,8 +12,8 @@ const FaqItem = ({ item, index }) => {
     <div className="relative z-2 mb-16">
       <div
         className="group relative flex cursor-pointer items-center justify-between gap-10 px-7"
-              onClick={() => {
-                setActiveId(activeId === item.id ? null : item.id);
+        onClick={() => {
+          setActiveId(activeId === item.id ? null : item.id);
         }}
       >
         <div className="flex-1">
@@ -28,21 +29,30 @@ const FaqItem = ({ item, index }) => {
           >
             {item.question}
           </div>
-              </div>
-              <div className={clsx('faq-icon flex size-12 items-center justify-center border-s2 shadow-400 rounded-full border-2 transition-all duration-500 group-hover:border-s4', active && 'before:bg-p1 after:rotate-0 after:bg-p1')}>
-                <div className="g4 size-11/12 rounded-full shadow-300"/>
-                   
-              </div>
+        </div>
+        <div className={clsx('faq-icon flex size-12 items-center justify-center border-s2 shadow-400 rounded-full border-2 transition-all duration-500 group-hover:border-s4', active && 'before:bg-p1 after:rotate-0 after:bg-p1')}>
+          <div className="g4 size-11/12 rounded-full shadow-300"/>
+        </div>
       </div>
       <SlideDown>
         {activeId === item.id && <div className="body-3 px-7 py-3.5">{item.answer}</div>}
       </SlideDown>
       <div className={clsx('g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 absolute', active && 'opacity-100')}>
         <div className='g4 absolute inset-0.5 -z-1 rounded-3xl' />
-        <div  className="absolute left-8 top-0 h-0.5 w-40 bg-p1"/>
+        <div className="absolute left-8 top-0 h-0.5 w-40 bg-p1"/>
       </div>
     </div>
   );
+};
+
+// Tambahkan validasi properti
+FaqItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    question: PropTypes.string.isRequired,
+    answer: PropTypes.string.isRequired,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default FaqItem;
